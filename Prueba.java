3cc8e8e7f8e6f754fdbdd969ba096b24dc7ae03f
AntonioV2 <AntonioLG@cobaltus.com>
2020-05-07 16:05:51 +0200

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("esto es una prueba de mierda para ver si compila esta mierda");
+        System.out.println("esto es una prueba de mierda para ver si compila esta mierda Pero ahora con Github");
     }
     
 }
